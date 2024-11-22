38fb3d4f81acc6b7c8cd0310afc5c84147c2c981
Rita <rita.helwangi@gmail.com>
2024-11-22 14:47:59 +0100

    added functions to game view

--- js/game.js
+++ js/game.js
@@ -2,6 +2,27 @@ let secretWord = '';
 let guessedLetters = [];
 let incorrectGuesses = []; 
 
+// List of words "EDIT LATER!"
+const words = ['javascript', 'python', 'html', 'css', 'nodejs', 'react'];
+
+function initializeGame() {
+  // Pick a random word from the list
+  secretWord = words[Math.floor(Math.random() * words.length)];
+
+  guessedLetters = [];
+  incorrectGuesses = [];
+  
+  updateWordDisplay();
+  updateIncorrectGuesses();
+  
+  // input and button
+  document.getElementById('guess-input').disabled = false;
+  document.getElementById('guess-btn').disabled = false;
+  document.getElementById('guess-input').value = '';  // Clear the input field
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
 	initializeGame();
 });
+
