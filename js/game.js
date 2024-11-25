e93b537f7621fad395ff70b34839d4847b9fdd0f
Rita <rita.helwangi@gmail.com>
2024-11-25 23:36:10 +0100

    wordlist to test/DOM element

--- js/game.js
+++ js/game.js
@@ -1,9 +1,29 @@
-let secretWord = ''; 
+// List of words "EDIT LATER!"
+const wordList = [
+  "JAVASCRIPT", "PROGRAMMING", "DEVELOPER", "HANGMAN", "CODING",
+  "SOFTWARE", "ALGORITHM", "DEBUGGING", "FUNCTION", "VARIABLE"
+];
+
+// DOM Elements
+const wordDisplay = document.getElementById("word-display");
+const incorrectGuessesDisplay = document.getElementById("incorrect-guesses");
+const guessInput = document.getElementById("guess-input");
+const guessButton = document.getElementById("guess-btn");
+const hangmanParts = [
+  document.getElementById("head"),
+  document.getElementById("body"),
+  document.getElementById("arms"),
+  document.getElementById("legs"),
+  document.getElementById("scaffold"),
+  document.getElementById("ground")
+];
+
+// Variables
+let wordToGuess = ''; 
 let guessedLetters = [];
 let incorrectGuesses = []; 
+const maxIncorrectGuesses = 6;
 
-// List of words "EDIT LATER!"
-const words = ['javascript', 'python', 'html', 'css', 'nodejs', 'react'];
 
 function initializeGame() {
   // Pick a random word from the list
