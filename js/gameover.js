acf4d42bb18f916f67f293bffa2cd5827e2abcaa
Hanna Seldén <hanna_lotta@hotmail.com>
2024-11-26 09:04:59 +0100

    kode uppdate+html classes added

--- js/gameover.js
+++ js/gameover.js
@@ -1,22 +1,17 @@
 
 //toggle vy
-/*
-document.querySelector('.show-loser-btn').addEventListener('click', () => {
-	document.querySelector('#lose').classList.remove('.hidden');
-})
 
-*/
 
 document.querySelector('.show-winner-btn').addEventListener('click', function() {
     // Visa vinnarsidan och dölja gameover-sidan
-    document.getElementById('win').classList.remove('hidden');
-    document.getElementById('lose').classList.add('hidden');
+    document.querySelector('#win').classList.remove('hidden');
+    document.querySelector('#lose').classList.add('hidden');
 });
 
 document.querySelector('.show-loser-btn').addEventListener('click', function() {
     // Visa gameover-sidan och dölja vinnarsidan
-    document.getElementById('lose').classList.remove('hidden');
-    document.getElementById('win').classList.add('hidden');
+    document.querySelector('#lose').classList.remove('hidden');
+    document.querySelector('#win').classList.add('hidden');
 });
 
 //boolean funcion win/lose
