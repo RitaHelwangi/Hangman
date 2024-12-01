25fe1fa47f2ebca2ac887aadbe15b10c9ae551b9
Rita <rita.helwangi@gmail.com>
2024-12-01 01:00:57 +0100

    changed id name to start/socre vy so they do not interfere with eachother

--- js/start.js
+++ js/start.js
@@ -1,5 +1,5 @@
 // Declare the selectedAvatar variable.
-let selectedAvatar = { head: '', difficulty: '' };
+let selectedAvatar = { headStart: '', difficulty: '' };
 
 // Add event listeners to all clickable avatar images.
 document.querySelectorAll('.clickable-image').forEach(img => {
